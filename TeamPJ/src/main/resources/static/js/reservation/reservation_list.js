db3243a4f9d16eba1cb0d39a082ca7a19beaffc0
dptmf1 <60639176+dptmftkdghk1@users.noreply.github.com>
2024-07-19 11:37:24 +0900

    대여신청 기능 완성

--- TeamPJ/src/main/resources/static/js/reservation/reservation_list.js
+++ TeamPJ/src/main/resources/static/js/reservation/reservation_list.js
@@ -1,20 +1,23 @@
 
 
 // 승인 클릭 시 승인 Status 업데이트
-function reservation_update(item){
-    fetch("/reservation/list",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            // "X-CSRF-TOKEN": csrfTokenInput.value
-        },
-        body: JSON.stringify(item)
-    }).then(response => {
-        if(response.ok){
-            alert('승인되었습니다.');
-            location.reload(); // 화면 새로고침
-        }
-    });
+function reservation_update(item, status){
+    if(status == null){
+        fetch("/reservation/list",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // "X-CSRF-TOKEN": csrfTokenInput.value
+            },
+            body: JSON.stringify(item)
+        }).then(response => {
+            if(response.ok){
+                alert('승인되었습니다.');
+                location.reload(); // 화면 새로고침
+            }
+        });
+    }
+
 }
 
 // 거부 클릭 시 칼럼 삭제
