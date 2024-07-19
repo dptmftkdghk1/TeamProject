2094bf6fb6948b48a471d73640e061884565cc74
kehe <kehe12@nate.com>
2024-07-19 18:04:24 +0900

    commit

--- TeamPJ/src/main/java/com/team/mapper/NoticeMapper.java
+++ TeamPJ/src/main/java/com/team/mapper/NoticeMapper.java
@@ -2,21 +2,26 @@ package com.team.mapper;
 
 
 import com.team.domain.NoticeDTO;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface NoticeMapper {
-    @Insert("INSERT INTO `board` (no, title, content) VALUES (NULL, #{title}, #{content})")
+    @Select("SELECT * FROM `board`")
+    List<NoticeDTO> selectAllNotice();
+
+    @Insert("INSERT INTO `board` (board_no, board_title, board_content) VALUES (NULL, #{boardTitle}, #{boardContent})")
     void insertNotice(NoticeDTO notice);
 
-    @Update("UPDATE board` SET no = #{no}, " +
-            "title = #{title}, content = #{content} " +
-            "WHERE no = #{no}")
+    @Update("UPDATE `board` SET " +
+            "board_title = #{boardTitle}, board_content = #{boardContent} " +
+            "WHERE board_no = #{no}")
     void updateNotice(NoticeDTO notice);
 
-    @Delete("DELETE FROM board` WHERE no = #{no}")
-    void deleteNotice(NoticeDTO notice);
+    @Delete("DELETE FROM `board` WHERE board_no = #{no}")
+    void deleteNotice(Long notice);
+
+
 }
