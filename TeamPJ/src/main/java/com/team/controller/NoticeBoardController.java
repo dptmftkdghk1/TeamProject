04c45307655550c911ad16d18081ac55227c2ec3
kehe <kehe12@nate.com>
2024-07-22 09:34:52 +0900

    Revert "Reapply "commit""
    This reverts commit 42122dcfb5c00aa276fdc3be2d3e13f402537e83.

--- TeamPJ/src/main/java/com/team/controller/NoticeBoardController.java
+++ TeamPJ/src/main/java/com/team/controller/NoticeBoardController.java
@@ -5,8 +5,12 @@ package com.team.controller;
 import com.team.domain.NoticeDTO;
 import com.team.mapper.NoticeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/Notice_Service")
@@ -17,27 +21,32 @@ public class NoticeBoardController {
 
 
     @GetMapping("/notice")
-    private String create_view(){
+    public String create_view(Model model){
+        List<NoticeDTO> notices = noticeMapper.selectAllNotice();
+        model.addAttribute("notices", notices);
+        System.out.println(notices);
         return "notice/notice";
     }
 
 
-//    @GetMapping("/registration")
-//    private String get_registration_view(){
-//        return "registration";
-//    }
-//
-//    @PostMapping("/registration")
-//    private String post_registration_view(NoticeDTO notice){
-//        noticeMapper.insertNotice(notice);
-//        return "registration";
-//    }
-//    @PatchMapping("notice/")
-//    public void updateMember(@RequestBody NoticeDTO updateMemberRequestDTO) {
-//        noticeMapper.updateNotice(updateMemberRequestDTO);
-//    }
-//    @DeleteMapping("/notice/{no}")
-//    public void deleteMember(@PathVariable Integer no) {
-//        noticeMapper.deleteNotice(no);
-//    }
+    @GetMapping("/registration")
+    private String get_registration_view(){
+        return "notice/registration";
+    }
+
+    @PostMapping("/registration")
+    private String post_registration_view(NoticeDTO notice){
+        noticeMapper.insertNotice(notice);
+        return "notice/registration";
+    }
+    @PutMapping("/notice/{no}")
+    public String updateMember(@RequestBody NoticeDTO updateMemberRequestDTO, @PathVariable("no") Integer no) {
+        noticeMapper.updateNotice(updateMemberRequestDTO);
+        return "redirect:/notice/notice";
+    }
+    @DeleteMapping("/notice/{id}")
+    public String delete(@PathVariable("id") Long No) {
+        noticeMapper.deleteNotice(No);
+        return "redirect:/";
+    }
 }
